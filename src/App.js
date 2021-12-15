4f656530994fc34ee5cc49e876bbe4b5c6b85e32
Danny Little <dannyl9979@gmail.com>
2021-12-14 20:20:02 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, useCallback } from 'react';
 
 function App() {
+  const [notes, setNotes] = useState('')
+  const [adj, setAdj] = useState('')
+  const [time, setTime] = useState('')
+  
+  const generate = useCallback(async () => {
+    const url = 'https://random-word-form.herokuapp.com/random/adjective';
+    const allNotes = ['A', 'B', 'C', 'D', 'E', 'F'];
+    const suffixes = ['', 'b', '#'];
+
+    const sample = arr => arr[~~(Math.random() * arr.length)];
+    const randNumInclusive = (low, high) =>
+      low + ~~(Math.random() * (high - low + 1));
+
+    const noteGen = () => {
+      const notesSet = new Set();
+      const n = randNumInclusive(2, 4);
+      while (notesSet.size < n) {
+        notesSet.add(sample(allNotes) + sample(suffixes));
+      }
+      return notesSet;
+    };
+
+    const adjGen = async () => {
+      const response = await fetch(url);
+      return await response.json();
+    };
+    const a = await adjGen()
+    setAdj(a)
+    setNotes(noteGen());
+    setTime(Math.ceil(randNumInclusive(5, 30)/5)*5);
+  }, []);
+
+  useEffect(() => {
+    generate()
+    }, [generate])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <h1>Free Improvisation</h1>
+        <p>Notes: {Array.from(notes).join(', ')}</p>
+        <p>Timeframe: {String(time)} seconds</p>
+        <p>Descriptor: {adj ? adj[0] : ''}</p>
+      </div>
+      <button onClick={generate}>Regenerate</button>
     </div>
   );
 }
